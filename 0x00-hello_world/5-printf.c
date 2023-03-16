9f7cc57b4ccf5f55eb0fc55794763ae4e23e37b7
myUsername <myemail@gmail.com>
2023-03-16 15:50:11 -0700

    Printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,11 @@
-print("with proper grammar, but the outcome is a piece of<F11>art.\n");
+#include <stdio.h>
+/**
+ * main -A program that prints a line using the printf function
+ * Return: 0 (Success)
+ /*
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n"
+return (0);
+}
