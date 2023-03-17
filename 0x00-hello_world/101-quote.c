3243c721bea45ce4d03f50716c360000889da11d
myUsername <myemail@gmail.com>
2023-03-16 18:55:51 -0700

    Quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,14 @@
-#include <stdio.h>
 #include <unistd.h>
 
 /**
- * main - A C program that prints a line in standard error
+ * main - prints a string to the standard error
  *
- * Return: 1 (Success)
+ * Return: Always 1 (Error)
  */
 int main(void)
 {
-        fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-        return (1);
+    write(STDERR_FILENO,
+          "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+          59);
+    return (1);
 }
-
