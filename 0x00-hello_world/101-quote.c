201d0d3118689a090afbde2a892d7092bf7bdf63
myUsername <myemail@gmail.com>
2023-03-16 18:44:34 -0700

    Quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - A c  program that prints a line in standard error
+ *
+ * Return: 1 (Success)
+ */
+int main(void)
+{
+	fput("and that piece of art is usefu\l" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}
