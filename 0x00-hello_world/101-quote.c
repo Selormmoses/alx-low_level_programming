cf04294019d2c4216406bebde642e4d90c460bf6
myUsername <myemail@gmail.com>
2023-03-19 03:09:58 -0700

    Quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - A c program thatt prints a line to the standard error
- * Return: 1 (Success)
+ * main - 101-quote.c
+ * description - Write a c program that prints exactly and that piece of
+ * art is useful
+ * Return: always 0 (Succes)
  */
-
 int main(void)
 {
-	fput("that piece of art is useful\" - Dora Korpar, 2015-10-19,\n", stdout);
-	return (0);
-}
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+
+	return (1);
