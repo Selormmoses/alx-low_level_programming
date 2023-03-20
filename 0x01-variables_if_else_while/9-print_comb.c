edacac157402cbcf204ae7a297dae1c8facd4136
myUsername <myemail@gmail.com>
2023-03-19 17:05:13 -0700

    Combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Print all possible combinations of singledigit numbers
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(',');
+		putchar(',');
+	}
+	putchar('\n');
+		return (0);
+}
