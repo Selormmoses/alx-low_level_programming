cfd1bfc9bd460bde8f2596dd6812a07e7c1e17ba
myUsername <myemail@gmail.com>
2023-03-22 22:10:21 -0700

    Islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -5,11 +5,11 @@
  * Return: 1 for lowercase character and 0 for anything else
  */
 
-int _islower(int c)i
-
+int _islower(int c)
 {
 	if (c >= 97 && c <= 122)
 	{
 		return (1);
-	} return (0);
+	}
+	return (0);
 }
