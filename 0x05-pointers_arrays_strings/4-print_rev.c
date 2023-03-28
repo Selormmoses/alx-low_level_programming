6be50c9cb2f636760719066cbcdbd18fddd868fe
myUsername <myemail@gmail.com>
2023-03-28 14:10:27 -0700

    7-puts_half.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,21 +7,21 @@
  */
 void print_rev(char *s)
 {
-	int longi = 0;
+        int longi = 0;
+        int i;
 
-	int 0;
+        while (*s != '\0')
+        {
+                longi++;
+                s++;
+        }
+        s--;
+        for (i = longi; i > 0; i--)
+        {
+                _putchar(*s);
+                s--;
+        }
 
-	while (*s != '\0')
-	{
-		longi++;
-		s++;
-	}
-	s--;
-	for (o = longi; o > 0; o--)
-	{
-		_putchar(*s);
-		s--;
-	}
-
-	_putchar('\n');
+        _putchar('\n');
 }
+
