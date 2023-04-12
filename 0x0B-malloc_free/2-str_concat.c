6c3309204cfd67166cc625d34271eded2054a6f2
myUsername <myemail@gmail.com>
2023-04-12 07:42:39 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,36 +1,50 @@
-#include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
 
+#include "main.h"
+#include <stdlib.h>
 /**
- * *str_concat - a funtion that concatenate two strings
- * @s1: input one
- * @s2: input two
- * Return: concatenate S1 and S2
+ * str_concat - get ends of input and add together for size
+ * @s1: input one to concat
+ * @s2: input two to concat
+ * Return: concat of s1 and s2
  */
 
 char *str_concat(char *s1, char *s2)
 {
-	char *conct;
-		int i, ci;
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
+	char *s3;
+	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
 
-	i = ci = 0;
-	while (s1[i] != '\0')
-		i++;
-	while (s2[ci] != '\0')
-	{
-		conct [i] = s1[i];
-		i++; }
+	while (s1 && s1[len1])
+		len1++;
+	while (s2 && s2[len2])
+		len2++;
 
-	while (s2[ci] != '\0')
+	s3 = malloc(sizeof(char) * (len1 + len2 + 1));
+	if (s3 == NULL)
+		return (NULL);
+
+	i = 0;
+	j = 0;
+
+	if (s1)
 	{
-		conct[i] = s2[ci];
-		i++, ci++;
+		while (i < len1)
+		{
+			s3[i] = s1[i];
+			i++;
+		}
 	}
-	conct[i] = '\0';
-	return (conct);
+
+	if (s2)
+	{
+		while (i < (len1 + len2))
+		{
+			s3[i] = s2[j];
+			i++;
+			j++;
+		}
+	}
+	s3[i] = '\0';
+
+	return (s3);
 }
+
