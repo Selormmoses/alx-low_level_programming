1db5a32f2719c1b107921bec5d872e16db056a1d
myUsername <myemail@gmail.com>
2023-04-12 07:32:22 -0700

    1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,31 +1,29 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-
-/*
- * _strdup - returns a pointer to a new sting
+#include "main.h"
+/**
+ * _strdup - duplicate to new memory space location
  * @str: char
  * Return: 0
  */
-
-char *_strup(char *str)
+char *_strdup(char *str)
 {
-	char *ccc;
+	char *aaa;
 	int i, r = 0;
 
 	if (str == NULL)
-	return (NULL);
-
+		return (NULL);
 	i = 0;
 	while (str[i] != '\0')
 		i++;
 
-	ccc = malloc(sizeof(char) * (i + 1));
-	if (ccc == NULL)
+	aaa = malloc(sizeof(char) * (i + 1));
+
+	if (aaa == NULL)
 		return (NULL);
 
-	for (r  = 0; str[r]; r++)
-		ccc[r] = str[r];
+	for (r = 0; str[r]; r++)
+		aaa[r] = str[r];
 
-	return (ccc);
+	return (aaa);
 }
